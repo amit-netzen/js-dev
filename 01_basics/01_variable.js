8b453c4c57fd2ae1722bf7f0e22f232570126832
amit-netzen <69844998+amit-netzen@users.noreply.github.com>
2026-03-22 12:13:15 +0000

    basics

---
+++ 01_basics/01_variable.js
@@ -0,0 +1,8 @@
+const account_id = 14527
+// used for creating a constant that cant be changed
+let accountemail= " amit@gmail.com"
+//used to define a variable
+account_name="email"
+var account_password ="amit1234"
+console.log(account_id);
+console.table([account_id , accountemail ,account_name,account_password])
